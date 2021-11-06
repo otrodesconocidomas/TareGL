94fc106d6f77901cff33ea9f3ce3d741eb9ca1e3
[otrodesconocidomas] <[rv17012@ues.edu.sv]>
2021-11-06 12:54:01 -0600

    Login correcto
    eso espero :"v

--- gestion_lab/grails-app/conf/application.groovy
+++ gestion_lab/grails-app/conf/application.groovy
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'gestion_lab.security.User'
@@ -8,15 +7,6 @@ grails.plugin.springsecurity.authority.className = 'gestion_lab.security.Role'
 
 grails.plugin.springsecurity.securityConfigType="InterceptUrlMap"
 grails.plugin.springsecurity.interceptUrlMap = [
-=======
-// Added by the Spring Security Core plugin:
-grails.plugin.springsecurity.userLookup.userDomainClassName = 'gestion_lab.SecUser'
-grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'gestion_lab.SecUserSecRole'
-grails.plugin.springsecurity.authority.className = 'gestion_lab.SecRole'
-grails.plugin.springsecurity.securityConfigType = "InterceptUrlMap"
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	
->>>>>>> 0f5496efbde1589bc4607e420e677a9616ea3fce
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
 	[pattern: '/index.gsp',      access: ['permitAll']],
@@ -25,16 +15,12 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-<<<<<<< HEAD
 	[pattern: '/**/favicon.ico', access: ['permitAll']],
 	[pattern: '/login/**',       access: ['permitAll']],
 	[pattern: '/logout/**',      access: ['permitAll']],
 	[pattern: '/dbconsole/**',   access: ['permitAll']],
 	[pattern: '/console/**',     access: ['permitAll']],
 	[pattern: '/', 				 access: ['ROLE_ADMIN']]
-=======
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
->>>>>>> 0f5496efbde1589bc4607e420e677a9616ea3fce
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
@@ -46,8 +32,5 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
-<<<<<<< HEAD
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.logout.afterLogoutUrl = '/login/auth'
-=======
->>>>>>> 0f5496efbde1589bc4607e420e677a9616ea3fce
--- gestion_lab/grails-app/conf/spring/resources.groovy
+++ gestion_lab/grails-app/conf/spring/resources.groovy
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import gestion_lab.security.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
-=======
-import gestion_lab.SecUserPasswordEncoderListener
-import org.example.SecUserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    secUserPasswordEncoderListener(SecUserPasswordEncoderListener)
-    secUserPasswordEncoderListener(SecUserPasswordEncoderListener)
->>>>>>> 0f5496efbde1589bc4607e420e677a9616ea3fce
 }
