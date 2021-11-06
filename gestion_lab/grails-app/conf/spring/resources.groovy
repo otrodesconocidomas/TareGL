7e4ea4424300abab6c5930bccd597c77314094df
ultimateIX <74211977+ultimateIX@users.noreply.github.com>
2021-11-06 11:28:32 -0600

    login primera parte

--- gestion_lab/grails-app/conf/spring/resources.groovy
+++ gestion_lab/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import gestion_lab.SecUserPasswordEncoderListener
+import org.example.SecUserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    secUserPasswordEncoderListener(SecUserPasswordEncoderListener)
+    secUserPasswordEncoderListener(SecUserPasswordEncoderListener)
 }
